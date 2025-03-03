0918eba81941b6ac898a9948a1921c08dc31f428
serme <Sermen88@yandex.ru>
2025-03-03 21:27:50 +0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         var dog = 8.0;      // тип double (из-за точки)
         var cat = 3.6;      // тип double
         var paper = 763789; // тип int
-
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper );
