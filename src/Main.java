39099ebdc3e26f0907d0f4222a5c7c12e0d4d666
serme <Sermen88@yandex.ru>
2025-03-03 22:27:05 +0500

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var totalWeight = boxer1 + boxer2;
+        var weightDifference = boxer2 - boxer1;
+        System.out.println(totalWeight);
+        System.out.println(weightDifference);
+        System.out.println("Общий вес двух бойцов = " + totalWeight + " кг");
+        System.out.println("Разница в весе бойцов = " + weightDifference + " кг");
 
 
 
