3b9de06c34c5e1a220bdcd0d27398d962f167f0d
serme <Sermen88@yandex.ru>
2025-03-03 22:38:45 +0500

    Task 5

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 
 public class Main {
     public static void main(String[] args) {
-        var boxer1 = 78.2;
-        var boxer2 = 82.7;
-        var totalWeight = boxer1 + boxer2;
-        var weightDifference = boxer2 - boxer1;
-        System.out.println(totalWeight);
-        System.out.println(weightDifference);
-        System.out.println("Общий вес двух бойцов = " + totalWeight + " кг");
-        System.out.println("Разница в весе бойцов = " + weightDifference + " кг");
-        var remainder = boxer2 % boxer1;
-        System.out.println(remainder);
-        System.out.println("Остаток от деления " + boxer2 + " на " + boxer1 + " = " + remainder);
+        var totalHours = 640;
+        var hoursPerEmployee = 8;
+        var totalEmployees = totalHours / hoursPerEmployee;
+
+        System.out.println(totalHours);
+        System.out.println("Всего работников в компании — " + totalEmployees + " человек.");
+
+        var increasedEmployees = totalEmployees + 94;
+        var newTotalHours = increasedEmployees * hoursPerEmployee;
+        System.out.println("Если в компании работает " + increasedEmployees + " человек, то всего "
+                + newTotalHours + " часов работы может быть поделено между сотрудниками.");
 
     }
 }
