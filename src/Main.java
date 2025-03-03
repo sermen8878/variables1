b7aea6a405b7fedff00c6c63ce6a421ef9bd497d
serme <Sermen88@yandex.ru>
2025-03-03 22:20:21 +0500

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 
 public class Main {
     public static void main(String[] args) {
-        var friend =19;
-        System.out.println(friend);
-        friend =friend+2;
-        System.out.println(friend);
-        friend =friend/7;
-        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
 
 
 
