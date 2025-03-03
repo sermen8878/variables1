917db6ef9cf30aa469cc8041bc456d5f12bcb33a
serme <Sermen88@yandex.ru>
2025-03-03 22:10:34 +0500

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        var dog =8.0-3.5;
-        var cat =3.6-1.6;
-        var paper =763789+7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        var friend =19;
+        System.out.println(friend);
+        friend =friend+2;
+        System.out.println(friend);
+        friend =friend/7;
+        System.out.println(friend);
+
+
 
     }
 }
